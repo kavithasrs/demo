e003b670eca2c5f0151ba6792b23d6e15fd1318e
kavitha <kavithasubbiah@hotmail.com>
2020-12-16 05:19:18 -0500

    JPA initial commit

--- src/main/java/com/kav/learn/persistence/demo/entities/Officer.java
+++ src/main/java/com/kav/learn/persistence/demo/entities/Officer.java
@@ -2,15 +2,28 @@ package com.kav.learn.persistence.demo.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
-@ToString
 @EqualsAndHashCode
+@Entity
+@Table(name = "officers")
 public class Officer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Enumerated(EnumType.STRING)
     private Rank rank;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    public Officer(){}
 
     public Officer(Rank rank, String firstName, String lastName) {
         this.rank = rank;
